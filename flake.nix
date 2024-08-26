ce6fd9511f527e7415cd1988b7485b5ddf77a4c5
N.O.Atomusk <orzkey@outlook.com>
2024-08-26 21:50:15 +0800

    去掉NUR

--- flake.nix
+++ flake.nix
@@ -3,23 +3,23 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nur = {
-      url = "github:Meow-2/NUR";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # nur = {
+    #   url = "github:Meow-2/NUR";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
     nixosConfigurations."HpZBook" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        ({
-          nixpkgs.overlays = [
-            (final: prev: {
-              myNUR = inputs.myNUR.packages."${prev.system}";
-            })
-          ];
-        })
+        # ({
+        #   nixpkgs.overlays = [
+        #     (final: prev: {
+        #       myNUR = inputs.myNUR.packages."${prev.system}";
+        #     })
+        #   ];
+        # })
         ./configuration.nix
       ];
     };
